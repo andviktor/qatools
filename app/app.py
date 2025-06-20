f6ac861728b5e8f977741ac2b9ad4dea969552cd
andviktor <and.viktor@gmail.com>
2025-06-20 21:52:49 +0200

    page title collection tool added.

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, Response
 from typing import Optional, Union, Any
 
-from modules.sitemap.sitemap import extract_internal_links, format_sitemap_for_jstree
+from app.modules.sitemap.sitemap import extract_internal_links, format_sitemap_for_jstree
+from app.modules.page_titles.page_titles_request import get_page_titles_request
+from app.modules.page_titles.page_titles_selenium import get_page_titles_selenium
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
-def index() -> Union[str, Response]:
+def sitemap() -> Union[str, Response]:
     url: Optional[str] = request.args.get("url")
     depth: int = request.args.get("depth", type=int, default=100)
     sitemap: Optional[Union[list[dict], str]] = None  # type: ignore
@@ -18,6 +20,19 @@ def index() -> Union[str, Response]:
             sitemap = format_sitemap_for_jstree(sitemap_data)
         except Exception as e:
             sitemap = f"An error occurred: {e}"
-        return render_template("index.html", url=url, depth=depth, sitemap=sitemap)
+        return render_template("pages/sitemap.html", url=url, depth=depth, sitemap=sitemap)
 
-    return render_template("index.html")
+    return render_template("pages/sitemap.html")
+
+
+@app.route("/page-titles", methods=["GET", "POST"])
+def page_titles() -> Union[str, Response]:
+    titles: Optional[dict[str, str]] = None
+    urls: Optional[list[str]] = request.form.get("urls")
+    enable_selenium: bool = "enable-selenium" in request.form
+    if urls:
+        urls = [url for url in urls.replace("\r", "").replace(" ", "").split("\n") if url]
+        get_page_titles = get_page_titles_selenium if enable_selenium else get_page_titles_request
+        titles = get_page_titles(urls)
+    
+    return render_template("pages/page_titles.html", urls=urls, titles=titles)
